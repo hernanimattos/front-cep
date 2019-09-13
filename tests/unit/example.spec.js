43f1f65228cd689c314598535d2f9ab0098607bf
Hernani <hernani.luiz@gmail.com>
2019-09-13 10:05:07 -0300

    modal

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,11 +1,11 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import error from '@/components/error.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
+    const errorMesssa = 'new message';
+    const wrapper = shallowMount(error, {
+      propsData: { error },
     });
     expect(wrapper.text()).toMatch(msg);
   });
