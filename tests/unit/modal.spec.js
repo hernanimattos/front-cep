de1167367cda43d5d3f130f6c11d60d10d72e2ed
Hernani <hernani.luiz@gmail.com>
2019-09-13 12:32:00 -0300

    test modal and error comoonent

--- tests/unit/modal.spec.js
+++ tests/unit/modal.spec.js
@@ -1,16 +1,43 @@
 import { shallowMount } from '@vue/test-utils';
 import modal from '@/components/modal.vue';
+import error from '@/components/error.vue';
 
-describe('error.vue', () => {
-  it('renders props.errorMessage when passed', () => {
-    const errorMessage = 'new message';
-
-    const wrapper = shallowMount(error, {
+describe('modal.vue', () => {
+  let wrapper;
+  let vm;
+  beforeEach(() => {
+    wrapper = shallowMount(modal, {
       propsData: {
-        errorMessage,
-
-       },
+        showData: true,
+        modalTitle: 'headerTitle',
+      },
     });
-    expect(wrapper.text()).toMatch(errorMessage);
+    vm = wrapper.vm; // eslint-disable-line
+    vm.$emit = jest.fn();
+  });
+
+  it('renders props.errorMessage when passed', () => {
+    expect(wrapper.find('p').text()).toBe('headerTitle');
+  });
+
+  it('renders props.showData when passed true', () => {
+    expect(wrapper.find('section').classes()).toContain('active');
+  });
+
+  it('renders props.showData when passed false', () => {
+    wrapper.setProps({
+      showData: false,
+    });
+    expect(wrapper.find('section').classes()).not.toContain('active');
+  });
+
+  it('check if use error', () => {
+    expect(wrapper.contains(error)).toBeTruthy();
+  });
+
+  it('teste close event emit', () => {
+    vm.close = jest.fn();
+    wrapper.find('section.wrapper-modal').trigger('click');
+    expect(vm.close).toBeCalled();
   });
 });
